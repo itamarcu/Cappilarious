4a232b74a855f8b76af1129c29a26e6dbb7b24bb
User <User@User-PC>
2017-01-20 13:42:54 +0200

    Waver update

--- Convolver/src/Waver.java
+++ Convolver/src/Waver.java
@@ -3,8 +3,26 @@ import java.awt.Color;
 public class Waver {
 	double cx, cy;
 	double speed;
+	double waveWidth;
 	double freq;
 	double timeSinceLastWave;
 	Color color;
 	
+	
+	public Waver (double centerX, double centerY, double speed, double waveWidth, double freq, Color color)
+	{
+		cx=centerX;
+		cy=centerY;
+		this.speed=speed;
+		this.waveWidth=waveWidth;
+		this.freq=freq;
+		this.color=color;
+		timeSinceLastWave=0;
+	}
+	public Wave generateWave ()
+	{
+		timeSinceLastWave=0;
+		Wave w= new Wave (cx, cy, speed, waveWidth, color);
+		return w;
+	}
 }
