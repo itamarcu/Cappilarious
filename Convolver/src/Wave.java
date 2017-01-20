fcc35e582efd250d8f3d0cf25ad357e7fe2c81f6
User <User@User-PC>
2017-01-20 14:11:37 +0200

    contains check

--- Convolver/src/Wave.java
+++ Convolver/src/Wave.java
@@ -3,18 +3,20 @@ import java.awt.Color;
 public class Wave {
 double cx, cy;
 double r1, r2;
+double width;
 double speed;
 Color color;
 public static Color pink, red;
 
-public Wave (double originX, double originY, double _speed, double width, Color _color)
+public Wave (double originX, double originY, double _speed, double _width, Color _color)
 {
 	cx=originX;
 	cy=originY;
-	this.speed=_speed;
-	this.r1=-width;
-	this.r2=0;
-	this.color=_color;
+	speed=_speed;
+	r1=-_width;
+	width=_width;
+	r2=0;
+	color=_color;
 }
 
 public void update (double dt)
@@ -22,5 +24,10 @@ public void update (double dt)
 	r1+=speed*dt;
 	r2+=speed*dt;
 	}
+public boolean contains (double x, double y)
+{
+	double dist=Math.pow(x-cx, 2);
+	return dist>=Math.pow(r1, 2)&&dist<=Math.pow(r2, 2);
+}
 }
 
